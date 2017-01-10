50383e44122bbfa31f78bda5347f032306c133c8
pyratebeard <root@pyratebeard.net>
2017-01-10 18:06:01 +0000

    removed email var. command with stderr and stdout to logfile, sends root a mail if non-zero return code. will only run if command not already running

--- job_scheduling.sh
+++ job_scheduling.sh
@@ -12,4 +12,9 @@
 date=$(date +%Y%m%d)
 time=$(date +%H%M%S)
 log_file="/var/log/do_something_"$date"_"$time".log"
-email="root@localhost"
+
+if [[ -z $(pgrep do_something) ]]; then
+	/usr/bin/do_something > $log_file 2>&1 || mail -s "do_something error" root@localhost < $log_file
+else
+	exit 1
+fi
