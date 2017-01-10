e28e02e3ed1d9e211245fdec46ae165b7f37f7f4
pyratebeard <root@pyratebeard.net>
2017-01-10 16:37:37 +0000

    header comments. variables declared.

--- job_scheduling.sh
+++ job_scheduling.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+#
+# wrapper script for running /usr/bin/do_something via cron
+#
+# program is run:
+#	mon-fri, every 15 minutes
+#	saturdays, once at noon
+#
+# author: dudley burrows
+#
+
+date=$(date +%Y%m%d)
+time=$(date +%H%M%S)
+log_file="/var/log/do_something_"$date"_"$time".log"
+email="root@localhost"
