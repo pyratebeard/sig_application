1107d29f74e5b3a3ddde007a0f314ad902db3a7c
pyratebeard <root@pyratebeard.net>
2017-01-12 15:59:05 +0000

    add in check for csv file without CRLF line terminators

--- shell_scripting.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# shell script to extract information from csv file
-#
-# requires 'Sample csv.csv'
-#
-# author: dudley burrows
-#
-
--- task2.sh
+++ task2.sh
@@ -9,5 +9,9 @@
 
 csv="sample.csv"
 
-printf "task 2: total non zero executions in each data centre are:\n"
-awk -F, '{print $2" "$3}' $csv | egrep -v "\ 0$" | cut -f 2 -d "_" | awk '{print $1}' | sort | uniq -c | awk '{print $2" with "$1" executions"}'
+if [ ! -f $csv ] ; then
+	printf "please run task1.sh first\n"
+else
+	printf "task 2: total non zero executions in each data centre are:\n"
+	awk -F, '{print $2" "$3}' $csv | egrep -v "\ 0$" | cut -f 2 -d "_" | awk '{print $1}' | sort | uniq -c | awk '{print $2" with "$1" executions"}'
+fi
