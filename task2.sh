8907abe06ae48a516aeefdc5c398e635b017dff8
pyratebeard <root@pyratebeard.net>
2017-01-12 15:14:52 +0000

    task 2 script

---
+++ task2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -
+#
+# task 2 - For each data center, report the total count of job executions where the error count was non zero, across all days.
+#
+# requires 'sample.csv'
+#
+# author: dudley burrows
+#
+
+csv="sample.csv"
+
+printf "task 2: total non zero executions in each data centre are:\n"
+awk -F, '{print $2" "$3}' $csv | egrep -v "\ 0$" | cut -f 2 -d "_" | awk '{print $1}' | sort | uniq -c | awk '{print $2" with "$1" executions"}'
